0efad9c81199d6352a4ba97cf041b5695b237ff2
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2022-07-13 09:00:09 +0900

    デフォルトコンポーネントを削除した(分割する必要がなくなった)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { Component } from './components/component';
-import { Default } from './components/Default/default';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 /*style*/
 import './style.css';
 import { Header } from './components/Default/Header/header';
+import { SideMenu } from './components/Default/SideMenu/sideMenu';
 
 export const App = () => {
     return (
         <div className="App">
-            <Header/>
+            <Header />
+            <div className="contents-wrapper">
+                <div className="main-contents">
+
+                </div>
+                <SideMenu />
+            </div>
             <BrowserRouter>
                 <Routes>
-                    {/* <Route path="/" element={<Default />} /> */}
                     <Route path="/component" element={<Component />} />
                     <Route path="/test" element={<h1>hogefuga</h1>} />
                 </Routes>
